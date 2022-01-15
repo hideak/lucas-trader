a7dfa3401cd7510bd1a71712bac1bfa9307704ec
hideak <whideak@gmail.com>
2022-01-15 14:44:40 +0000

    Deploying to gh-pages from  @ 9123af4217fbce505b1641c1e5b0edd7a3ba437e 🚀

---
+++ precache-manifest.aa9bfb4b5a86555fe59e515c8db46257.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908a51be5411ca044940",
+    "url": "/css/app.b569265f.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "aa138f1e74a354c602e75aee427eba37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12dd6f298fb24f81a2cc",
+    "url": "/js/about.56f828f0.js"
+  },
+  {
+    "revision": "908a51be5411ca044940",
+    "url": "/js/app.b3b46019.js"
+  },
+  {
+    "revision": "cbb7332c451785b88d28",
+    "url": "/js/chunk-vendors.e214f800.js"
+  },
+  {
+    "revision": "4ccad91926a6e3abd713585ec10360bb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
